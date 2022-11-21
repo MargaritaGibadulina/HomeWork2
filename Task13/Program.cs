bfdbaf2232882a500481fb12bd5b88d6371756ac
Margarita Gibaduina <patokinam@yandex.ru>
2022-11-21 22:14:07 +0300

    add task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+int number = ReadInt("Введите число: ");
+if (ThirdDigit(number) == -1)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра {ThirdDigit(number)}");
+}
+
+int ThirdDigit(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result; 
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
